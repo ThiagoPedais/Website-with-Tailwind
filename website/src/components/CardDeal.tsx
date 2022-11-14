9fc7c9078eeb523e9d140c7fc2249f6150f2112f
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-11-14 19:46:52 -0300

    feedback and testimonials components

--- website/src/components/CardDeal.tsx
+++ website/src/components/CardDeal.tsx
@@ -5,7 +5,20 @@ import Button from "./Button";
 
 
 const CardDeal = () => (
-    <section>CardDeal</section>
+    <section className={layout.section}>
+      <div className={ layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a bettter card deal<br className="sm:block hidden"/> in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, molestiae et! Asperiores laudantium minima id, eius quaerat consectetur saepe velit placeat ducimus nostrum qui voluptatem deleniti non beatae atque laboriosam.
+        </p>
+
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%] " />
+      </div>
+    </section>
   )
 
 
