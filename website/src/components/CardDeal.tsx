99c55273132ddf5a1e921e1dbe95404f458ee894
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-11-14 19:11:29 -0300

    Stats, Button, Business, Billing components

--- website/src/components/CardDeal.tsx
+++ website/src/components/CardDeal.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { card } from "../assets/images";
+import styles, { layout } from "../styles";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
+
+
+const CardDeal = () => (
+    <section>CardDeal</section>
   )
-}
 
-export default CardDeal
+
+export default CardDeal;
